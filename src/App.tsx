434108cd5142c37c1917567d53fcf7fffbf2a92f
Toma-Shops-247 <support@tomashops.com>
2025-05-28 17:11:43 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4 text-xl text-center text-blue-600">
+      Welcome to TomaShops 🛒
+    </div>
+  );
+}
+
+export default App;
